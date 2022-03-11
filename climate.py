347aa7a53b44f3635610d1d304f3f115a02be14d
Frank Ziegler <funtastix@googlemail.com>
2022-03-11 16:25:17 +1100

    add more missing imports

--- climate.py
+++ climate.py
@@ -56,6 +56,7 @@ from .const import *
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
 from homeassistant.const import PRECISION_HALVES, PRECISION_TENTHS
+from numbers import Number
 
 SUPPORT_FLAGS_MAIN = SUPPORT_TARGET_TEMPERATURE | SUPPORT_PRESET_MODE
 SUPPORT_FLAGS_ZONE = SUPPORT_TARGET_TEMPERATURE | SUPPORT_PRESET_MODE
