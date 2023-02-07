ea8ed566a7c9868e6dbb4d804fe434cd62cda1a8
Funtastix <funtastix@github.com>
2023-02-07 23:17:20 +1100

    wrapping try except around startup hass error

--- custom_components/rinnaitouch/climate.py
+++ custom_components/rinnaitouch/climate.py
@@ -114,7 +114,11 @@ class RinnaiTouch(ClimateEntity):
     def system_updated(self):
         """After system is updated write the new state to HA."""
         self.update_external_temperature()
-        self.schedule_update_ha_state()
+        #this very infrequently fails on startup so wrapping in try except
+        try:
+            self.schedule_update_ha_state()
+        except:
+            pass 
 
     @property
     def supported_features(self):
